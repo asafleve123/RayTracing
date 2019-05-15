e60a77a3e40220e70a5324a4e42c786fe934030b
utanji <utanji@g.jct.ac.il>
2019-05-15 20:20:02 +0300

    utanji first succesfull commit

---
+++ Tests/primitives/VectorTest.java
@@ -0,0 +1,36 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void scale() {
+    }
+
+    @Test
+    void length() {
+    }
+
+    @Test
+    void normalize() {
+    }
+
+    @Test
+    void dotProduct() {
+    }
+
+    @Test
+    void crossProduct() {
+    }
+}
